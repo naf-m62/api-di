b8f03ba7abd851a8990dde01f4f926feeeb9b50e
Dinar Nafikov <d.nafikov@fbs.com>
2020-02-05 19:41:14 +0300

    add templates

--- apps/server/server.go
+++ apps/server/server.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"context"
+	"html/template"
 	"net/http"
 	"os"
 	"os/signal"
@@ -33,12 +34,17 @@ func Start(app di.Container) {
 		)
 	}
 
-	r.HandleFunc("/", m(handlers.GetLinkListHandler)).Methods("GET")
+	getHand := handlers.Handler{
+		Tmpl: template.Must(template.ParseGlob("./apps/handlers/templates/*")),
+	}
+
+	r.HandleFunc("/", m(getHand.GetLinkListHandler)).Methods("GET")
+	r.HandleFunc("/create", m(getHand.CreateLinkForm)).Methods("GET")
+	r.HandleFunc("/links/{id}", m(getHand.UpdateLinkForm)).Methods("GET")
+
 	r.HandleFunc("/create", m(handlers.CreateLinkHandler)).Methods("POST")
-	r.HandleFunc("/links/{id}", m(handlers.GetLinkHandler)).Methods("GET")
-	r.HandleFunc("/links/{id}", m(handlers.UpdateLinkHandler)).Methods("PUT")
+	r.HandleFunc("/links/{id}", m(handlers.UpdateLinkHandler)).Methods("POST")
 	r.HandleFunc("/links/{id}", m(handlers.DeleteLinkHandler)).Methods("DELETE")
-	//r.HandleFunc("/cars/{carId}", m(handlers.DeleteCarHandler)).Methods("DELETE")
 
 	srv := &http.Server{
 		Handler:      r,
